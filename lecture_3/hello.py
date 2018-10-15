67146d93dff369431f039c206bb17bf55a299196
HellWiz <alod-2355@mail.ru>
2018-10-15 20:26:57 +0300

    работа на лекции

---
+++ lecture_3/hello.py
@@ -0,0 +1,13 @@
+#Magic, который пишем чаще всего. Что он делает?
+#import numpy
+#import matplotlib
+#from matplotlib import pylab, mlab, pyplot
+#np = numpy
+#plt = pyplot
+#from IPython.display import display
+#from IPython.core.pylabtools import figsize, getfigs
+#from pylab import *
+#from numpy import *
+
+#и все это 1 строчкой! inline указывает, что вывод графиков будет в ноутбук (подробнее в документации: выбор движка)
+%pylab inline
